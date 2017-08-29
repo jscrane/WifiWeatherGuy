fd3f7b5b7a9832c4c215d53d44e5377bc475720d
Stephen Crane <jscrane@gmail.com>
2017-08-29 09:57:08 +0100

    initial

---
+++ WiFiWeatherGuy.ino
@@ -0,0 +1,426 @@
+#include <ArduinoJson.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <TFT_ILI9163C.h>
+#include <ESP8266WiFi.h>
+#include <time.h>
+#include <FS.h>
+
+char ssid[32];
+char password[32];
+char key[16];
+char station[32];
+bool metric;
+const char host[] = "api.wunderground.com";
+
+#define BLACK   0x0000
+#define WHITE   0xFFFF
+
+#define CS D6
+#define DC D8
+
+TFT_ILI9163C tft = TFT_ILI9163C(CS, DC);
+
+//Print &out = tft;
+Print &out = Serial;
+#define DEBUG
+
+const size_t bufferSize = JSON_OBJECT_SIZE(0) + 9 * JSON_OBJECT_SIZE(2) + 2 * JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(8) + JSON_OBJECT_SIZE(9) + JSON_OBJECT_SIZE(12) + JSON_OBJECT_SIZE(56) + 2530;
+DynamicJsonBuffer jsonBuffer(bufferSize);
+
+uint32_t next_fetch = 0;
+uint16_t update_interval;
+
+void setup() {
+
+  tft.begin();
+  tft.setTextColor(WHITE, BLACK);
+
+  Serial.begin(115200);
+
+  bool result = SPIFFS.begin();
+  out.print("SPIFFS: ");
+  out.println(result);
+  if (!result)
+    return;
+
+  File f = SPIFFS.open("/config.txt", "r");
+  if (!f) {
+    out.print(F("file.open!"));
+    return;
+  }
+  char buf[512];
+  f.readBytes(buf, sizeof(buf));
+  char *b = buf, *p = strsep(&b, "=");
+  while (p) {
+    if (strcmp(p, "ssid") == 0)
+      strcpy(ssid, strsep(&b, "\n"));
+    else if (strcmp(p, "password") == 0)
+      strcpy(password, strsep(&b, "\n"));
+    else if (strcmp(p, "key") == 0)
+      strcpy(key, strsep(&b, "\n"));
+    else if (strcmp(p, "station") == 0)
+      strcpy(station, strsep(&b, "\n"));
+    else if (strcmp(p, "update") == 0)
+      update_interval = atoi(strsep(&b, "\n"));
+    else if (strcmp(p, "metric") == 0)
+      metric = (bool)atoi(strsep(&b, "\n"));
+    p = strsep(&b, "=");
+  }
+  f.close();
+
+#ifdef DEBUG
+  out.print(F("ssid: "));
+  out.println(ssid);
+  out.print(F("password: "));
+  out.println(password);
+  out.print(F("key: "));
+  out.println(key);
+  out.print(F("station: "));
+  out.println(station);
+  out.print(F("update: "));
+  out.println(update_interval);
+  out.print(F("metric: "));
+  out.println(metric);
+#endif
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    out.print(F("."));
+  }
+#ifdef DEBUG  
+  out.println();
+  out.print(F("Connected to "));
+  out.println(ssid);
+  out.println(WiFi.localIP());
+#endif
+}
+
+const char *icon;
+const char *weather;
+int temp, feelslike;
+char temp_unit;
+const char *humidity;
+int wind;
+const char *wind_unit;
+int atmos_pressure;
+const char *pres_unit;
+const char *pressure_trend;
+const char *wind_dir;
+const char *sunrise_hour;
+const char *sunrise_minute;
+const char *sunset_hour;
+const char *sunset_minute;
+time_t epoch;
+const char *city;
+int wind_degrees;
+
+// These read 16- and 32-bit types from the SD card file.
+// BMP data is stored little-endian, Arduino is little-endian too.
+// May need to reverse subscript order if porting elsewhere.
+static uint16_t read16(File &f) {
+  uint16_t result;
+  size_t n = f.read((uint8_t *)&result, sizeof(result));
+  return result;
+}
+
+static uint32_t read32(File &f) {
+  uint32_t result;
+  size_t n = f.read((uint8_t *)&result, sizeof(result));
+  return result;
+}
+
+// from Adafruit's spitftbitmap ST7735 example
+int bmp_draw(const char *filename, uint8_t x, uint8_t y) {
+  int      bmpWidth, bmpHeight;   // W+H in pixels
+  uint8_t  bmpDepth;              // Bit depth (currently must be 24)
+  uint32_t bmpImageoffset;        // Start of image data in file
+  uint32_t rowSize;               // Not always = bmpWidth; may have padding
+  uint8_t buf[60];
+  uint8_t  buffidx = sizeof(buf);      // Current position in buffer
+  boolean  flip    = true;        // BMP is stored bottom-to-top
+  int      w, h, row, col;
+  uint8_t  r, g, b;
+  uint32_t pos = 0;
+
+  if((x >= tft.width()) || (y >= tft.height())) return -1;
+
+#ifdef DEBUG
+  uint32_t startTime = millis();
+#endif
+
+  char fbuf[32];
+  strcpy(fbuf, "/");
+  strcat(fbuf, filename);
+  strcat(fbuf, ".bmp");
+  File f = SPIFFS.open(fbuf, "r");
+  if (!f) {
+    out.print(F("file.open!"));
+    out.print(' ');
+    out.println(filename);
+    return -1;
+  }
+
+  // Parse BMP header
+  if (read16(f) != 0x4D42) {
+    out.println(F("Unknown BMP signature"));
+    f.close();
+    return -1;
+  }
+#ifdef DEBUG  
+  out.print(F("File size: "));
+  out.println(read32(f));
+#else
+  (void)read32(f);
+#endif  
+  (void)read32(f); // Read & ignore creator bytes
+  bmpImageoffset = read32(f); // Start of image data
+#ifdef DEBUG
+  out.print(F("Image Offset: ")); 
+  out.println(bmpImageoffset, DEC);
+#endif
+  // Read DIB header
+#ifdef DEBUG
+  uint32_t header_size = read32(f);
+  out.print(F("Header size: ")); 
+  out.println(header_size);
+#else
+  (void)read32(f);
+#endif
+  bmpWidth  = read32(f);
+  bmpHeight = read32(f);
+  if (read16(f) != 1) {
+    out.println(F("# planes -- must be '1'"));
+    f.close();
+    return -1;
+  }
+  bmpDepth = read16(f); // bits per pixel
+#ifdef DEBUG
+  out.print(F("Bit Depth: ")); 
+  out.println(bmpDepth);
+#endif
+  if((bmpDepth != 24) || (read32(f) != 0)) {
+    // 0 = uncompressed
+    out.println(F("BMP format not recognized."));
+    f.close();
+    return -1; 
+  }
+  
+#ifdef DEBUG
+  out.print(F("Image size: "));
+  out.print(bmpWidth);
+  out.print('x');
+  out.println(bmpHeight);
+#endif
+  
+  // BMP rows are padded (if needed) to 4-byte boundary
+  rowSize = (bmpWidth * 3 + 3) & ~3;
+  
+  // If bmpHeight is negative, image is in top-down order.
+  // This is not canon but has been observed in the wild.
+  if (bmpHeight < 0) {
+    bmpHeight = -bmpHeight;
+    flip      = false;
+  }
+  
+  // Crop area to be loaded
+  w = bmpWidth;
+  h = bmpHeight;
+  if ((x+w-1) >= tft.width())  w = tft.width()  - x;
+  if ((y+h-1) >= tft.height()) h = tft.height() - y;
+  
+  // Set TFT address window to clipped image bounds
+  tft.setAddrWindow(x, y, x+w-1, y+h-1);
+  
+  for (row=0; row<h; row++) { // For each scanline...
+
+    // Seek to start of scan line.  It might seem labor-
+    // intensive to be doing this on every line, but this
+    // method covers a lot of gritty details like cropping
+    // and scanline padding.  Also, the seek only takes
+    // place if the file position actually needs to change
+    // (avoids a lot of cluster math in SD library).
+    if (flip) // Bitmap is stored bottom-to-top order (normal BMP)
+      pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
+    else     // Bitmap is stored top-to-bottom
+      pos = bmpImageoffset + row * rowSize;
+      
+    if (f.position() != pos) { // Need seek?
+      f.seek(pos);
+      buffidx = sizeof(buf); // Force buffer reload
+    }
+  
+    for (col=0; col<w; col++) { // For each pixel...
+      // Time to read more pixel data?
+      if (buffidx >= sizeof(buf)) { // Indeed
+        f.read(buf, sizeof(buf));
+        buffidx = 0; // Set index to beginning
+      }
+  
+      // Convert pixel from BMP to TFT format, push to display
+      b = buf[buffidx++];
+      g = buf[buffidx++];
+      r = buf[buffidx++];
+
+      tft.pushColor(tft.Color565(r,g,b));
+    } // end pixel
+  }
+  f.close();
+#ifdef DEBUG
+  out.print(F("Loaded in "));
+  out.print(millis() - startTime);
+  out.println(F(" ms"));
+#endif
+  return 0;
+}
+
+static int centre_text(const char *s, int x, int size)
+{
+  return x - (strlen(s) * size * 6) / 2;
+}
+
+static int right(int n, int x, int size)
+{
+  return x - n * size * 6;
+}
+
+static int val_len(int b)
+{
+  if (b >= 1000) return 4;
+  if (b >= 100) return 3;
+  if (b >= 10) return 2;
+  if (b >= 0) return 1;
+  if (b > -10) return 2;
+  return 3;
+}
+
+void update_display() {
+
+  tft.fillScreen(WHITE);
+  tft.setTextColor(BLACK);
+  tft.setTextSize(2);
+  tft.setCursor(1, 1);
+  tft.print(wind);
+  tft.setTextSize(1);
+  tft.print(wind_unit);
+  tft.setCursor(1, 17);
+  tft.print(wind_dir);
+
+  tft.setTextSize(2);
+  tft.setCursor(1, tft.height() - 16);
+  tft.print(temp);
+  tft.setTextSize(1);
+  tft.print(temp_unit);
+  if (temp != feelslike) {
+    tft.setCursor(1, tft.height() - 24);
+    tft.print(feelslike);
+  }
+
+  tft.setTextSize(2);
+  int h = atoi(humidity);
+  tft.setCursor(right(val_len(h), tft.width(), 2) - 6, tft.height() - 16);
+  tft.print(h);
+  tft.setTextSize(1);
+  tft.setCursor(tft.width() - 6, tft.height() - 16);
+  tft.print('%');
+
+  tft.setTextSize(2);
+  tft.setCursor(right(val_len(atmos_pressure), tft.width(), 2)-6*strlen(pres_unit), 1);
+  tft.print(atmos_pressure);
+  tft.setTextSize(1);
+  tft.print(pres_unit);
+  int atmos_rising = atoi(pressure_trend);
+  if (atmos_rising == 1) {
+    tft.setCursor(right(6, tft.width(), 1), 17);
+    tft.print(F("rising"));
+  } else if (atmos_rising == -1) {
+    tft.setCursor(right(7, tft.width(), 1), 17);
+    tft.print(F("falling"));
+  }
+  
+  tft.setCursor(centre_text(city, tft.width()/2, 1), 25);
+  tft.print(city);
+  tft.setCursor(centre_text(weather, tft.width()/2, 1), 34);
+  tft.print(weather);
+  bmp_draw(icon, tft.width()/2 - 25, 42);
+  char buf[32];
+  strftime(buf, sizeof(buf), metric? "%H:%S": "%I:%S%p", localtime(&epoch));
+  tft.setCursor(centre_text(buf, tft.width()/2, 1), 109);
+  tft.print(buf);
+  strftime(buf, sizeof(buf), "%a %d", localtime(&epoch));
+  tft.setCursor(centre_text(buf, tft.width()/2, 1), 118);
+  tft.print(buf);
+
+  // http://www.iquilezles.org/www/articles/sincos/sincos.htm
+  int rad = 60, cx = 80, cy = 64;
+  const float a = 0.999847695, b = 0.017452406;
+  // wind dir is azimuthal angle with N at 0
+  float s = 1.0, c = 0.0;
+  for (uint16_t i = 0; i < wind_degrees; i++) {
+    const float ns = a*s + b*c;
+    const float nc = a*c - b*s;
+    c = nc;
+    s = ns;
+  }
+  // wind dir rotates clockwise so compensate
+  int ex = cx-rad*c, ey = cy-rad*s;
+  tft.fillCircle(ex, ey, 3, BLACK);
+  tft.drawLine(ex, ey, ex + wind*(cx - ex)/50, ey+wind*(cy-ey)/50, BLACK);
+}
+
+void loop() {
+
+  uint32_t now = millis();
+  if (now > next_fetch) {
+    next_fetch = now + update_interval * 1000L;
+    WiFiClient client;
+    if (client.connect(host, 80)) {
+      client.print(String("GET /api/") + key + "/astronomy/conditions/q/" + station
+                   + ".json HTTP/1.1\r\n"
+                   + "Host: " + host + "\r\n"
+                   + "Connection: close\r\n"
+                   + "\r\n");
+      if (client.connected()) {
+        client.find("\r\n\r\n");
+        JsonObject &root = jsonBuffer.parseObject(client);
+        JsonObject& current_observation = root["current_observation"];
+        const char *local_epoch = current_observation["local_epoch"];
+        epoch = (time_t)atoi(local_epoch);
+        icon = current_observation["icon"];
+        weather = current_observation["weather"];
+        if (metric) {
+          temp = current_observation["temp_c"];
+          feelslike = atoi(current_observation["feelslike_c"]);
+          temp_unit = 'C';
+          wind = current_observation["wind_kph"];
+          wind_unit = "kph";
+          atmos_pressure = atoi(current_observation["pressure_mb"]);
+          pres_unit = "mb";
+        } else {
+          temp = current_observation["temp_f"];
+          feelslike = atoi(current_observation["feelslike_f"]);
+          temp_unit = 'F';          
+          wind = current_observation["wind_mph"];
+          wind_unit = "mph";
+          atmos_pressure = atoi(current_observation["pressure_in"]);
+          pres_unit = "in";
+        }
+        humidity = current_observation["relative_humidity"];
+        pressure_trend = current_observation["pressure_trend"];
+        wind_dir = current_observation["wind_dir"];
+        wind_degrees = current_observation["wind_degrees"];
+        city = current_observation["observation_location"]["city"];
+        sunrise_hour = root["sun_phase"]["sunrise"]["hour"];
+        sunrise_minute = root["sun_phase"]["sunrise"]["minute"];
+        sunset_hour = root["sun_phase"]["sunset"]["hour"];
+        sunset_minute = root["sun_phase"]["sunset"]["minute"];
+        update_display();
+      }
+      client.stop();
+    } else
+      out.println(F("Connection failed!"));
+  }
+
+}
