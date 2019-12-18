8b85457abe746695ef29720b1def193dc1809617
Stephen Crane <jscrane@gmail.com>
2019-12-18 14:49:58 +0000

    missing file

---
+++ zone.h
@@ -0,0 +1,8 @@
+#ifndef _ZONE_H_
+#define _ZONE_H_
+
+// define your timezone here, see https://github.com/JChristensen/Timezone
+TimeChangeRule summer = {"IST", Last, Sun, Mar, 2, 60};
+TimeChangeRule winter = {"GMT", Last, Sun, Oct, 1, 0};
+
+#endif
